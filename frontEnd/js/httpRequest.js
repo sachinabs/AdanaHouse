15913d683e22a55486cc8559b105863a23a85d35
NavaneethL <navaneeth0906@gmail.com>
2021-10-28 12:09:24 +0530

    1 http request updated

---
+++ frontEnd/js/httpRequest.js
@@ -0,0 +1,14 @@
+function showCount() {
+
+    let date = document.getElementById("view-count-date").value;
+    alert(date);
+    let httpRequest = new XMLHttpRequest();
+    httpRequest.onreadystatechange() = function () {
+        if (this.readyState == 4 && this.status == 200) {
+            console.log(this.response);
+        }
+    }
+    httpRequest.open("GET", "localhost:1011/counts?Date=" + date, true);
+    httpRequest.send();
+
+}
